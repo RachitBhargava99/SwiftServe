214b72ba8f1c1a2c8c973ee501f76fe9aef1ba2e
Rachit <rachit@ibm.com>
2020-10-17 15:05:00 -0400

    Order Creation Feature added - to be tested

---
+++ api/stores/routes.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from db import schemas
+from db.db import get_db
+from .controllers import create_menu_item
+from db.schemas import User
+from etc.usrmng import fastapi_users
+
+router = APIRouter()
+
+
+@router.post('', response_model=schemas.Item)
+def create_group_route(item: schemas.ItemCreate, db: Session = Depends(get_db), user: User = Depends(fastapi_users.get_current_user)):
+    print(user)
+    if not user.is_owner:
+        raise HTTPException(403, detail="The user is not an owner")
+    db_item = create_menu_item(db, str(user.id), item.name, item.description, item.price)
+    return db_item
